583b803a46ca2a94033c79039adebde938e12ead
Kamogelo44 <kamogeloobakeng44@gmail.com>
2026-07-18 16:02:06 +0200

    Add Category entity and IDE config
    Add a JPA Category entity (src/main/java/co/za/obcodes/local_service_directory_api/model/Category.java) with @Entity/@Table, autogenerated id and a unique, non-null name column. Also add NetBeans shared project config (nb-configuration.xml) enabling compile-on-save and setting JDK platform hint.

---
+++ src/main/java/co/za/obcodes/local_service_directory_api/model/Category.java
@@ -0,0 +1,43 @@
+package co.za.obcodes.local_service_directory_api.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import jakarta.persistence.Column;
+
+@Entity
+@Table(name = "categories")
+public class Category {
+    
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    
+    @Column(nullable = false, unique = true, length = 100)
+    private String name;
+    
+    public Category() {
+    }
+    
+    public Category(String name) {
+        this.name = name;
+    }
+    
+    public Long getId() {
+        return id;
+    }
+    
+    public void setId(Long id) {
+        this.id = id;
+    }
+    
+    public String getName() {
+        return name;
+    }
+    
+    public void setName(String name) {
+        this.name = name;
+    }
+}
